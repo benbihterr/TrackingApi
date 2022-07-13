09b84b0cbc67459de7b7e1115398878e3c9844c3
benbihterr <bihter.ulu04@gmail.com>
2022-07-13 22:26:45 +0300

    update

--- TrackingApi/Controllers/IssueController.cs
+++ TrackingApi/Controllers/IssueController.cs
@@ -13,11 +13,11 @@ namespace TrackingApi.Controllers
         private readonly IssueDbContext _context;
 
         public IssueController(IssueDbContext context) => _context = context;
-        
+
         [HttpGet]
-        public async Task<IEnumerable<Issue>> Get() 
+        public async Task<IEnumerable<Issue>> Get()
             => await _context.Issues.ToListAsync();
-      
+
         [HttpGet("id")]
         [ProducesResponseType(typeof(Issue), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -34,8 +34,25 @@ namespace TrackingApi.Controllers
             await _context.Issues.AddAsync(issue);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction(nameof(GetById), new {id = issue.Id}, issue);
+            return CreatedAtAction(nameof(GetById), new { id = issue.Id }, issue);
         }
 
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]        
+        public async Task<IActionResult> Update(int id, Issue issue)
+        {
+            if (id != issue.Id) return BadRequest();
+
+            _context.Entry(issue).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        
     }
+
 }
