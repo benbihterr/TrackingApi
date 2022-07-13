7c518a3b952ccf8e065ba668842d7daaab2a044f
benbihterr <bihter.ulu04@gmail.com>
2022-07-13 21:40:32 +0300

    404 not found

---
+++ TrackingApi/Controllers/IssueController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TrackingApi.Data;
+using TrackingApi.Models;
+
+namespace TrackingApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : ControllerBase
+    {
+        private readonly IssueDbContext _context;
+
+        public IssueController(IssueDbContext context) => _context = context;
+        
+        [HttpGet]
+        public async Task<IEnumerable<Issue>> Get() 
+            => await _context.Issues.ToListAsync();
+      
+        [HttpGet("id")]
+        [ProducesResponseType(typeof(Issue), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var issue = await _context.Issues.FindAsync(id);
+            return issue == null ? NotFound() : Ok(issue);
+        }
+
+
+    }
+}
